7fd3701298235a51d3cee4f7486cc7b2436a6c29
Bob Amand <biotmg@nc.rr.com>
2015-12-15 11:58:03 -0500

    direct py code for car

--- car.py
+++ car.py
@@ -17,18 +17,57 @@ print("  The y intercept = {}".format(regr1.intercept_))
 print("\n          The Rsq = {}".format(regr1.score(df[['Mileage']],
                                                     df[['Price']])))
 
-plt.scatter(df.Mileage, df.Price)
-plt.ylabel("Price")
-plt.xlabel("Mileage")
-plt.title("Price x Mileage for Used Cars")
-plt.plot(df[['Mileage']],
-         regr1.predict(df[['Mileage']]),
-         color='red',
-         linewidth=2)
-plt.show()
+'''
+The plot interupts the flow so 'commented-out' for now.
+'''
+
+# plt.scatter(df.Mileage, df.Price)
+# plt.ylabel("Price")
+# plt.xlabel("Mileage")
+# plt.title("Price x Mileage for Used Cars")
+# plt.plot(df[['Mileage']],
+#          regr1.predict(df[['Mileage']]),
+#          color='red',
+#          linewidth=2)
+# plt.show()
 
 print("Mileage alone is not a good predictor of price.")
-print("The Rsq at {0:.2f} is very low. Only {1:.2f} of Price variance is captured in Mileage.".
-      format(regr1.score(df[['Mileage']], df[['Price']]),
-             regr1.score(df[['Mileage']], df[['Price']])))
-print("The current model is insufficient to predict price.")
+print("Only {0:.6f} of Price variance is captured in Mileage.".
+      format(regr1.score(df[['Mileage']], df[['Price']])))
+
+print("The current model is insufficient to predict price.\n")
+
+fulldf = df[['Price', 'Mileage', 'Cylinder', 'Liter',
+             'Doors', 'Cruise', 'Sound', 'Leather']]
+
+input = fulldf[['Mileage', 'Cylinder', 'Liter',
+                'Doors', 'Cruise', 'Sound', 'Leather']]
+output = fulldf['Price']
+
+regrm = linear_model.LinearRegression()
+regrm.fit(input, output)
+print("Coefficients for Mileage, Cylinder, Liter, Doors, Cruise, Sound, leather:\n")
+print(regrm.coef_)
+print("\n")
+print("Y-intercept = {0:.4f}".format(regrm.intercept_))
+print("    The Rsq = {0:.4f}".format(regrm.score(input, output)))
+
+''' Full correlation '''
+
+fulldf.corr()
+
+'''Based on highest correlation with Price '''
+
+subdf = df[['Price', 'Mileage', 'Cylinder', 'Cruise']]
+
+input = fulldf[['Mileage', 'Cylinder', 'Cruise']]
+output = fulldf['Price']
+
+regrm = linear_model.LinearRegression()
+regrm.fit(input, output)
+print("\n")
+print("Coefficients for Mileage, Cylinder + Cruise:")
+print("\n")
+print(regrm.coef_)
+print("Y-intercept = {0:.4f}".format(regrm.intercept_))
+print("    The Rsq = {0:.4f}".format(regrm.score(input, output)))
---
+++ car1.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+import itertools
+
+
+fulldf = pd.read_csv("car_data.csv")
+possible_columns = ['Mileage', 'Cylinder', 'Liter',
+                    'Doors', 'Cruise', 'Sound', 'Leather']
+
+
+def high_value_regression(fulldf, possible_columns):
+
+    '''
+combos=[]
+- the range "1" allows control of the minimum number of columns to be included.
+- The 'intertools' appears to create a list of tuples for each combination of columns.
+- Each tuple needs to be unpacked to a list prior to evaluation by LinearRegression.
+    '''
+    combos = []
+    for x in range(1, len(possible_columns)):
+        combos.append(list(itertools.combinations(possible_columns, x)))
+    # print(len(possible_columns))
+    # print(combos)
+    '''
+letter=[]
+   - Each tuple from 'combo' is unpacked to a list and appended into letter.
+    '''
+    letter = []
+    for x in combos:
+        for y in x:
+            letter.append(list(y))
+    print("printing letter list:")
+    for s in range(0, 20):
+        print(letter[s]),
+
+    '''
+results=[]
+   - The list of column names are plugged into df[x] as input_data for regression.
+   - After each loop through the list of lists:
+       o The 'results' list accummulates two columns: 'Grouping' and 'Score'.
+       o The list of column names are 'join'd and appended into 'Grouping'.
+       o The Rsq is recorded as 'Score' in the DataFrame.
+   - The table is sorted high to low and 5 top printed.
+
+    '''
+    results = []
+    output = fulldf['Price']
+
+    for x in letter:
+        input_data = fulldf[x]
+
+        regrm = linear_model.LinearRegression()
+        regrm.fit(input_data, output)
+        regrm.coef_
+        regrm.intercept_
+
+        results.append([', '.join(x), regrm.score(input_data, output)])
+
+    results = pd.DataFrame(results, columns=('Grouping', 'Score'))
+
+    return results.sort_index(by='Score', ascending=False).head(5)
+
+high_value_regression(fulldf, possible_columns)
+
+# print("Oddly, cannot discover why the initial 7 column Rsq (including 'liter) = higher than max in table!!")
+# print("Rsq all 7 = 0.44626")
